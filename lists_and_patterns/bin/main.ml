2147ff38b967ae2237d1bfe038698c5c1138f464
Hans-J. Schmid <knock@myopendoor.de>
2023-02-07 05:08:32 +0100

    updates lists and patterns.

--- lists_and_patterns/bin/main.ml
+++ lists_and_patterns/bin/main.ml
@@ -1,11 +1,10 @@
 open Base
 open Base.Poly
-(* open Core_bench (* uncomment to run benchmark tests *) *)
+open Fn
 open Stdio
+(* open Core_bench (* uncomment to run benchmark tests *) *)
 
 let () = print_endline ""
-let _r = [ 1; 2; 3 ]
-let _r = [ 1; 2; 3 ]
 let _r = [ 1; 2; 3 ]
 let empty = []
 let _r = 3 :: empty
@@ -19,8 +18,8 @@ let _r = sum []
 (* let rec drop_value l to_drop =
        match l with
        | [] -> []
-       | to_drop :: tl -> drop_value tl to_drop
-       | hd :: tl -> hd :: drop_value tl to_drop
+       | to_drop :: tl -> drop_value tl to_drop   (* no pattern-matching on to_drop *)
+       | hd :: tl -> hd :: drop_value tl to_drop  (* unused *)
 
    Line 5, characters 7-15:
    Warning 11 [redundant-case]: this match case is unused. *)
@@ -32,6 +31,13 @@ let rec drop_value l to_drop =
       let new_tl = drop_value tl to_drop in
       if hd = to_drop then new_tl else hd :: new_tl
 
+(* using guards *)
+let rec drop_value' l to_drop =
+  match l with
+  | [] -> []
+  | hd :: tl when hd = to_drop -> drop_value' tl to_drop
+  | hd :: tl -> hd :: drop_value' tl to_drop
+
 let rec _drop_zero l =
   match l with
   | [] -> []
@@ -39,6 +45,7 @@ let rec _drop_zero l =
   | hd :: tl -> hd :: _drop_zero tl
 
 let _r = drop_value [ 1; 2; 3 ] 2
+let _r = drop_value' [ 1; 2; 3 ] 2
 
 let _plus_one_match x =
   match x with
@@ -59,14 +66,15 @@ let _plus_one_if x =
   else if x = 5 then 6
   else x + 1
 
+(* pattern-matching is fast *)
 (* uncomment to run benchmark tests *)
 
 (* let _r =
-  [
-    Bench.Test.create ~name:"plus_one_match" (fun () -> _plus_one_match 10);
-    Bench.Test.create ~name:"plus_one_if" (fun () -> _plus_one_if 10);
-  ]
-  |> Bench.bench *)
+   [
+     Bench.Test.create ~name:"plus_one_match" (fun () -> _plus_one_match 10);
+     Bench.Test.create ~name:"plus_one_if" (fun () -> _plus_one_if 10);
+   ]
+   |> Bench.bench *)
 
 (* Estimated testing time 20s (2 benchmarks x 10s). Change using -quota SECS.
    ┌────────────────┬──────────┐
@@ -81,10 +89,10 @@ let rec _sum_if l =
 
 (* uncomment to run benchmark tests *)
 (* let _r =
-  let _numbers = List.range 0 1000 in
-    [ Bench.Test.create ~name:"sum_if" (fun () -> _sum_if _numbers);
-      Bench.Test.create ~name:"sum"    (fun () -> sum _numbers) ]
-    |> Bench.bench *)
+   let _numbers = List.range 0 1000 in
+     [ Bench.Test.create ~name:"sum_if" (fun () -> _sum_if _numbers);
+       Bench.Test.create ~name:"sum"    (fun () -> sum _numbers) ]
+     |> Bench.bench *)
 
 (* Estimated testing time 20s (2 benchmarks x 10s). Change using -quota SECS.
    ┌────────┬──────────┐
@@ -110,23 +118,31 @@ let max_widths header rows =
       List.map2_exn ~f:Int.max acc (lengths row))
 
 let pad s length = s ^ String.make (length - String.length s) ' '
+let _r = pad "hello" 10 (* "hello     " *)
 
 let render_row row widths =
   let padded = List.map2_exn row widths ~f:pad in
   "| " ^ String.concat ~sep:" | " padded ^ " |"
 
+(* - : string = "| Hello      | World           |" *)
+let _r = render_row [ "Hello"; "World" ] [ 10; 15 ]
+
 let render_separator widths =
   let pieces = List.map widths ~f:(fun w -> String.make w '-') in
   "|-" ^ String.concat ~sep:"-+-" pieces ^ "-|"
 
+let _r = render_separator [ 3; 6; 2 ] (* "|-----+--------+----|" *)
+
 let render_table header rows =
   let widths = max_widths header rows in
   String.concat ~sep:"\n"
-    (render_row header widths :: render_separator widths
-    :: List.map rows ~f:(fun row -> render_row row widths))
+    (render_separator widths :: render_row header widths
+     :: render_separator widths
+     :: List.map rows ~f:(fun row -> render_row row widths)
+    @ [ render_separator widths ])
 
 let _r =
-  Stdio.print_endline
+  print_endline
     (render_table
        [ "language"; "architect"; "first release" ]
        [
@@ -144,15 +160,8 @@ let _r = List.map2_exn ~f:Int.max [ 1; 2; 3 ] [ 3; 2; 1 ]
 
 let _r = List.fold ~init:0 ~f:( + ) [ 1; 2; 3; 4 ]
 let _r = List.fold ~init:[] ~f:(fun acc hd -> hd :: acc) [ 1; 2; 3; 4 ]
-let _r = render_separator [ 3; 6; 2 ]
-(* - : string = "|-----+--------+----|" *)
-
 let _s = "." ^ "." ^ "." ^ "." ^ "." ^ "." ^ "."
 let _s = String.concat [ "."; "."; "."; "."; "."; "."; "." ]
-let _r = pad "hello" 10
-let _r = render_row [ "Hello"; "World" ] [ 10; 15 ]
-(* - : string = "| Hello      | World           |" *)
-
 let _r = List.reduce ~f:( + ) [ 1; 2; 3; 4; 5 ]
 let _r = List.reduce ~f:( + ) []
 let _r = List.filter ~f:(fun x -> x % 2 = 0) [ 1; 2; 3; 4; 5 ]
@@ -200,9 +209,8 @@ let rec _ls_rec s =
 
 let rec length = function [] -> 0 | _ :: tl -> 1 + length tl
 let _r = length [ 1; 2; 3 ]
-let make_list n = List.init n ~f:(fun x -> x)
+let make_list n = List.init n ~f:id
 let _r = length (make_list 10)
-(* - : int = 10 *)
 
 (* let _r = length (make_list 10_000_000) *)
 (* Stack overflow during evaluation (looping recursion?). *)
@@ -212,10 +220,7 @@ let rec length_plus_n l n =
 
 let length l = length_plus_n l 0
 let _r = length [ 1; 2; 3; 4 ]
-(* - : int = 4 *)
-
 let _r = length (make_list 10_000_000)
-(* - : int = 10000000 *)
 
 let rec _remove_sequential_duplicates list =
   match list with
@@ -225,6 +230,7 @@ let rec _remove_sequential_duplicates list =
       if first = second then _remove_sequential_duplicates (second :: tl)
       else first :: _remove_sequential_duplicates (second :: tl)
 
+(* using as patterns *)
 let rec _remove_sequential_duplicates = function
   | [] as l -> l
   | [ _ ] as l -> l
@@ -232,6 +238,7 @@ let rec _remove_sequential_duplicates = function
       if first = second then _remove_sequential_duplicates tl
       else first :: _remove_sequential_duplicates tl
 
+(* using or pattern *)
 let rec _remove_sequential_duplicates list =
   match list with
   | ([] | [ _ ]) as l -> l
@@ -239,6 +246,7 @@ let rec _remove_sequential_duplicates list =
       if first = second then _remove_sequential_duplicates tl
       else first :: _remove_sequential_duplicates tl
 
+(* using guards *)
 let rec _remove_sequential_duplicates list =
   match list with
   | ([] | [ _ ]) as l -> l
@@ -250,20 +258,10 @@ let rec _remove_sequential_duplicates list =
 let _r = "foo" = "bar"
 let _r = 3 = 4
 let _r = [ 1; 2; 3 ] = [ 1; 2; 3 ]
-
-(* # (=) - : 'a -> 'a -> bool = <fun> *)
-
-let rec remove_sequential_duplicates list =
-  match list with
-  | ([] | [ _ ]) as l -> l
-  | first :: (second :: _ as tl) when first = second ->
-      remove_sequential_duplicates tl
-  | first :: tl -> first :: remove_sequential_duplicates tl
-
-let _r = remove_sequential_duplicates [ 1; 2; 2; 3; 4; 3; 3 ]
+let _r = _remove_sequential_duplicates [ 1; 2; 2; 3; 4; 3; 3 ]
 (* - : int list = [1; 2; 3; 4; 3] *)
 
-let _r = remove_sequential_duplicates [ "one"; "two"; "two"; "two"; "three" ]
+let _r = _remove_sequential_duplicates [ "one"; "two"; "two"; "two"; "three" ]
 (* - : string list = ["one"; "two"; "three"] *)
 
 (* let _r = (fun x -> x + 1) = (fun x -> x + 1) *)
@@ -283,14 +281,14 @@ let _r = remove_sequential_duplicates [ "one"; "two"; "two"; "two"; "three" ]
     | x :: tl when Option.is_some x -> 1 + count_some tl
     | x :: tl -> -1 (* unreachable *) *)
 
-let rec _count_some list =
-  match list with
+let rec _count_some l =
+  match l with
   | [] -> 0
   | x :: tl when Option.is_none x -> _count_some tl
   | _ :: tl -> 1 + _count_some tl
 
-let rec _count_some list =
-  match list with
+let rec _count_some l =
+  match l with
   | [] -> 0
   | None :: tl -> _count_some tl
   | Some _ :: tl -> 1 + _count_some tl
